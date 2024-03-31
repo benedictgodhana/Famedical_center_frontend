6c69cf7f0cb38edf4ee24ef54f8e97dac08ceb0a
benedictgodhana <benedictgodhana7@gmail.com>
2024-03-31 13:57:48 +0300

    sixth

--- src/components/EmployeeSidebar.vue
+++ src/components/EmployeeSidebar.vue
@@ -4,7 +4,7 @@
         permanent
       >
         <v-list nav>
-          <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" value="inbox" to="/employee" color="white" active-class="active-button"></v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" value="inbox" to="/doctor" color="white" active-class="active-button"></v-list-item>
           <v-list-item prepend-icon="mdi-clock-time-five" title="Manage Appointments" value="supervisors" color="white"  to="/employee/manage-bookings" active-class="active-button"></v-list-item>
           <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"   elevation="0"></v-list-item>
         </v-list>
